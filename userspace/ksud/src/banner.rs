8915d912da94ebcb796c58adf9e6b19d7d320da2
dre <134717165+dre698@users.noreply.github.com>
2026-08-29 16:17:03 +0700

    Update banner to print 'NADEKO' instead of 'NadekoSU'

--- userspace/ksud/src/banner.rs
+++ userspace/ksud/src/banner.rs
@@ -3,5 +3,5 @@ use figlet_rs::FIGlet;
 pub fn print_banner() -> String {
     let logo_printer = FIGlet::small().unwrap();
 
-    logo_printer.convert("NadekoSU").unwrap().to_string()
+    logo_printer.convert("NADEKO").unwrap().to_string()
 }
